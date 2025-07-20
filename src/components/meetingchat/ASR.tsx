843f9807132cbac7c55c7370413349172d11f283
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-07-20 16:31:45 +0900

    レイアウト修正

--- src/components/meetingchat/ASR.tsx
+++ src/components/meetingchat/ASR.tsx
@@ -5,31 +5,31 @@ interface Props{
     engine: ASREngine;
 }
 export function ASR({engine}: Props){
-  const [enabled, setEnabled] = useState(engine.isRecognizing());
-  const onEnabledChange: ChangeEventHandler<HTMLInputElement> = e=>{
-    if(e.currentTarget.checked){
-        engine.start();
-        setEnabled(true);
-    } else{
-        engine.stop();
-    }
-  };
-  const onKeepEnabledChange: ChangeEventHandler<HTMLInputElement> = e=>{
-    engine.setKeepEnabled(e.currentTarget.checked);    
-  };
-  const onEngineFinished = ()=>{
-      setEnabled(false);
-  };
-  useEffect(()=>{
-    engine.addEventListener("finished", onEngineFinished);
-    return ()=>{
-      engine.removeEventListener("finished", onEngineFinished);
+    const [enabled, setEnabled] = useState(engine.isRecognizing());
+    const onEnabledChange: ChangeEventHandler<HTMLInputElement> = e=>{
+        if(e.currentTarget.checked){
+            engine.start();
+            setEnabled(true);
+        } else{
+            engine.stop();
+        }
     };
-  },[]);
-  return <div>
-    <label style={{marginBottom: 0}}>
-      <input type="checkbox" checked={enabled} onChange={onEnabledChange} />音声認識</label>&nbsp;
-    <label style={{marginBottom: 0}}>
-      <input type="checkbox" defaultChecked={engine.isKeepEnabled()} onChange={onKeepEnabledChange} />onにし続ける(最長1時間)</label>&nbsp;
-  </div>;
+    const onKeepEnabledChange: ChangeEventHandler<HTMLInputElement> = e=>{
+        engine.setKeepEnabled(e.currentTarget.checked);    
+    };
+    const onEngineFinished = ()=>{
+        setEnabled(false);
+    };
+    useEffect(()=>{
+        engine.addEventListener("finished", onEngineFinished);
+        return ()=>{
+            engine.removeEventListener("finished", onEngineFinished);
+        };
+    },[]);
+    return <div>
+        <label style={{marginBottom: 0}}>
+            <input type="checkbox" checked={enabled} onChange={onEnabledChange} />音声認識</label>&nbsp;
+        <label style={{marginBottom: 0}}>
+            <input type="checkbox" defaultChecked={engine.isKeepEnabled()} onChange={onKeepEnabledChange} />onにし続ける(最長1時間)</label>&nbsp;
+    </div>;
 }
