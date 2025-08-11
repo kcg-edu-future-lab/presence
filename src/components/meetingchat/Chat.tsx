847c25c53c570ae73cbec4f94e0737ea97fde230
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-08-12 00:21:25 +0900

    #10

--- src/components/meetingchat/Chat.tsx
+++ src/components/meetingchat/Chat.tsx
@@ -1,6 +1,9 @@
 import { FormEventHandler, useEffect, useRef } from "react";
-import { Button, TextField } from "@mui/material";
-import { ChatLogs } from "./model/ChatLogs";
+import { Box, Button, Grid, IconButton, TextField } from "@mui/material";
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { ChatLogs, Log } from "./model/ChatLogs";
+import { copyTextToClipboard, downloadTextByAnchorTag } from "../../util/Util";
 
 interface Props {
     logs: ChatLogs;
@@ -9,6 +12,7 @@ interface Props {
 export function Chat({logs, onChat}: Props){
     const sentenceInputRef = useRef<HTMLInputElement>(null!);
     const chatLogDivRef = useRef<HTMLDivElement>(null!);
+
     const onSubmit: FormEventHandler = e=>{
         e.preventDefault();
         const sentence = sentenceInputRef.current.value.trim();
@@ -16,6 +20,15 @@ export function Chat({logs, onChat}: Props){
         onChat(sentenceInputRef.current.value);
         sentenceInputRef.current.value = "";
     };
+    const onCopyClick: FormEventHandler = e=>{
+        e.preventDefault();
+        copyTextToClipboard(chatLogsToText(logs.getLogs()));
+    };
+    const onDownloadClick: FormEventHandler = e=>{
+        e.preventDefault();
+        downloadTextByAnchorTag("chat.txt", chatLogsToText(logs.getLogs()));
+    };
+
     useEffect(()=>{
         const d = chatLogDivRef.current;
         d.scrollTo({
@@ -23,21 +36,41 @@ export function Chat({logs, onChat}: Props){
             behavior: 'smooth'
         });
     });
-    return <div>
-        <form onSubmit={onSubmit}>
-            <div>
-                <TextField size="small" inputRef={sentenceInputRef} />
-                <Button type="submit">送信</Button>
-            </div>
-        </form>
-        <div ref={chatLogDivRef} style={{overflow: "scroll", resize: "vertical",
-            height: "200px", border: "solid 1px", padding: 4, borderRadius: "4px"}}>
-            <span>
-                { logs.getLogs().map((l, li)=><div key={li}>
-                    <span>{l.senderName}</span>:&nbsp;
+
+    return <Grid container>
+        <Grid size={10}>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <TextField size="small" inputRef={sentenceInputRef} />
+                    <Button type="submit">送信</Button>
+                </div>
+            </form>
+        </Grid>
+        <Grid size={2}>
+            <Box display="flex" justifyContent="flex-end">
+                <IconButton onClick={onCopyClick}>
+                    <ContentCopyIcon/>
+                </IconButton>
+                <IconButton onClick={onDownloadClick}>
+                    <CloudDownloadIcon/>
+                </IconButton>
+            </Box>
+        </Grid>
+        <Grid size={12}>
+            <div ref={chatLogDivRef} style={{overflow: "scroll", resize: "vertical",
+                height: "200px", border: "solid 1px", padding: 4, borderRadius: "4px"}}>
+                <span>
+                    { logs.getLogs().map((l, li)=><div key={li}>
+                        <span>{l.senderName}</span>:&nbsp;
                     { l.sentence }
-                </div>)}
-            </span>
-        </div>
-    </div>;
+                    </div>)}
+                </span>
+            </div>
+        </Grid>
+    </Grid>;
+}
+
+function chatLogsToText(logs: Log[]){
+    return logs.reduce((prev, current)=>
+        prev + `[${current.senderName}] ${current.sentence}\n`, "");
 }
--- src/util/Util.ts
+++ src/util/Util.ts
@@ -14,3 +14,37 @@ export function computeIfAbsentMap<T, U>(map: Map<T, U>, key: T, comp: Function)
     map.set(key, value);
     return value;
 }
+
+export function copyTextToClipboard(content: string){
+    const item = new ClipboardItem({
+        "text/plain": content
+    });
+    navigator.clipboard.write([item]);
+}
+
+export function downloadTextByAnchorTag(filename: string, content: string){
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(
+        new Blob([content],
+            { type: "text/plain"}));
+    a.setAttribute("download", filename);
+    a.dispatchEvent(new MouseEvent("click"));
+}
+
+export function copyCanvasImageToClipboard(
+    canvas: HTMLCanvasElement, contentType: string = "image/png"){
+    canvas.toBlob(blob=>{
+        const content: {[key: string]: Blob} = {};
+        content[contentType] = blob!;
+        const item = new ClipboardItem(content);
+        navigator.clipboard.write([item]);
+    });
+}
+
+export function downloadCanvasImageByAnchorTag(
+    filename: string, canvas: HTMLCanvasElement, contentType: string = "image/png"){
+    const a = document.createElement("a");
+    a.href = canvas.toDataURL(contentType);
+    a.setAttribute("download", filename);
+    a.dispatchEvent(new MouseEvent("click"));
+}
